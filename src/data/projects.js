d82605f8d7ebdb7a31c588d4da7def58569578de
Dico Diaz <dicodiaz@gmail.com>
2022-12-10 19:04:40 -0500

    update projects liveVersion data

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ const projects = [
     desc: 'Single Page App (SPA) that shows up-to-date worldwide data about covid-19 cases.',
     img: 'https://github.com/dicodiaz/covid-metrics/blob/develop/app_screenshot-2.png?raw=true',
     langs: ['React', 'Redux', 'Bootstrap'],
-    liveVersion: 'https://dicodiaz-covid-metrics.netlify.app/',
+    liveVersion: 'https://covid-metrics.dicodiaz.com.co',
     sourceCode: 'https://github.com/dicodiaz/covid-metrics',
   },
   {
@@ -16,7 +16,7 @@ const projects = [
     desc: 'Single Page App (SPA) that allows users to make simple calculations and to read a random math-related quote.',
     img: 'https://github.com/dicodiaz/math-magicians/blob/develop/app_screenshot-2.png?raw=true',
     langs: ['React', 'Redux', 'Bootstrap'],
-    liveVersion: 'https://dicodiaz.me/math-magicians/',
+    liveVersion: 'https://math-magicians.dicodiaz.com.co',
     sourceCode: 'https://github.com/dicodiaz/math-magicians',
   },
   {
@@ -25,7 +25,7 @@ const projects = [
     desc: 'A quick tool to look for any TV show. Built based on 2 external APIs: TvMaze API and Involvement API.',
     img: 'https://github.com/dicodiaz/tv-shows-finder/blob/develop/app_screenshot-2.png?raw=true',
     langs: ['JavaScript', 'Webpack', 'Bootstrap', 'HTML', 'CSS'],
-    liveVersion: 'https://dicodiaz.me/tv-shows-finder/',
+    liveVersion: 'https://tv-shows-finder.dicodiaz.com.co',
     sourceCode: 'https://github.com/dicodiaz/tv-shows-finder',
   },
   {
@@ -34,7 +34,7 @@ const projects = [
     desc: 'Mock online bookstore that features thunks to consume a Bookstore API.',
     img: 'https://github.com/dicodiaz/bookstore/blob/develop/app_screenshot-2.png?raw=true',
     langs: ['React', 'Redux', 'Bootstrap'],
-    liveVersion: 'https://dicodiaz.me/bookstore/',
+    liveVersion: 'https://bookstore.dicodiaz.com.co',
     sourceCode: 'https://github.com/dicodiaz/bookstore',
   },
 ];
