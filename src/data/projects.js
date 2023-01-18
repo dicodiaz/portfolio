9f7d2d42cd87d495a2aad128a425525013e0a0c4
Dico Diaz <dicodiaz@gmail.com>
2023-01-17 23:21:30 -0500

    update projects and skills

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,21 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 const projects = [
   {
     id: uuidv4(),
-    title: 'Covid Metrics',
-    desc: 'Single Page App (SPA) that shows up-to-date worldwide data about covid-19 cases.',
+    title: 'Financial Modeling',
+    desc: 'SPA that shows up-to-date worldwide data about stock prices.',
     img: 'https://github.com/dicodiaz/covid-metrics/blob/develop/app_screenshot-2.png?raw=true',
     langs: ['React', 'Redux', 'Bootstrap'],
-    liveVersion: 'https://covid-metrics.dicodiaz.com.co',
-    sourceCode: 'https://github.com/dicodiaz/covid-metrics',
-  },
-  {
-    id: uuidv4(),
-    title: 'Math Magicians',
-    desc: 'Single Page App (SPA) that allows users to make simple calculations and to read a random math-related quote.',
-    img: 'https://github.com/dicodiaz/math-magicians/blob/develop/app_screenshot-2.png?raw=true',
-    langs: ['React', 'Redux', 'Bootstrap'],
-    liveVersion: 'https://math-magicians.dicodiaz.com.co',
-    sourceCode: 'https://github.com/dicodiaz/math-magicians',
+    liveVersion: 'https://financial-modeling.dicodiaz.com.co/',
+    sourceCode: 'https://github.com/dicodiaz/financial-modeling',
   },
   {
     id: uuidv4(),
@@ -25,17 +16,8 @@ const projects = [
     desc: 'A quick tool to look for any TV show. Built based on 2 external APIs: TvMaze API and Involvement API.',
     img: 'https://github.com/dicodiaz/tv-shows-finder/blob/develop/app_screenshot-2.png?raw=true',
     langs: ['JavaScript', 'Webpack', 'Bootstrap', 'HTML', 'CSS'],
-    liveVersion: 'https://tv-shows-finder.dicodiaz.com.co',
+    liveVersion: 'https://tv-shows-finder.dicodiaz.com.co/',
     sourceCode: 'https://github.com/dicodiaz/tv-shows-finder',
-  },
-  {
-    id: uuidv4(),
-    title: 'Bookstore',
-    desc: 'Mock online bookstore that features thunks to consume a Bookstore API.',
-    img: 'https://github.com/dicodiaz/bookstore/blob/develop/app_screenshot-2.png?raw=true',
-    langs: ['React', 'Redux', 'Bootstrap'],
-    liveVersion: 'https://bookstore.dicodiaz.com.co',
-    sourceCode: 'https://github.com/dicodiaz/bookstore',
   },
 ];
 
--- src/data/skills.js
+++ src/data/skills.js
@@ -3,24 +3,29 @@ import { BsCircleFill, BsDiamondFill, BsSquareFill } from 'react-icons/bs';
 const skills = {
   'Front-End': {
     Bullet: BsDiamondFill,
-    list: ['JavaScript', 'React', 'Redux', 'Bootstrap', 'HTML5', 'CSS3', 'Jest'],
+    list: [
+      'TypeScript',
+      'JavaScript',
+      'React',
+      'Redux',
+      'GraphQL',
+      'Bootstrap',
+      'HTML5',
+      'CSS3',
+      'Jest',
+      'React Testing Library',
+      'Enzyme',
+      'Cypress',
+      'Kotlin',
+    ],
   },
   'Tools & Methods': {
     Bullet: BsSquareFill,
-    list: [
-      'Git',
-      'Gitflow',
-      'GitHub',
-      'Heroku',
-      'Netlify',
-      'TDD',
-      'Responsive Development',
-      'Chrome Dev Tools',
-    ],
+    list: ['Git', 'GitHub', 'Netlify', 'TDD', 'Responsive Development', 'Chrome Dev Tools'],
   },
   'Soft Skills': {
     Bullet: BsCircleFill,
-    list: ['Remote Pair-Programming', 'Teamwork', 'Leadership', 'Mentoring'],
+    list: ['Remote Pair-Programming', 'Teamwork', 'Leadership', 'Mentoring', 'Proactivity'],
   },
 };
 
