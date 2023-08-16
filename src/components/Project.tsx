a69bf10170656713a03566ede034595a43febf64
Dico Diaz <dicodiaz@gmail.com>
2023-08-15 19:04:08 -0500

    Fix project column count and positioning

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -14,7 +14,7 @@ const Project: FC<ProjectProps> = ({ data, image, onSeeProjectClick, delay }) =>
   const { title, description, languages } = data;
 
   return (
-    <Col md={6} lg={4} xl={3} className="project px-md-2">
+    <Col md={6} lg={4} className="project px-md-2">
       <motion.div
         initial={{ opacity: 0, x: 200 }}
         whileInView={{ opacity: 1, x: 0 }}
